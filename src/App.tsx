c72f32c1d51d2ff4f1ab13942d4db533436b80c7
phantom99a1 <103617452+phantom99a1@users.noreply.github.com>
2023-04-16 15:02:40 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Signup from './signups/Signup';
+
+function App() {
+  return (
+    <div className="App">
+      <Signup/>
+    </div>
+  );
+}
+
+export default App;
